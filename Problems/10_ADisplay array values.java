c00a70c8be1d35ecd57704ca4fb1d961c9b6a41d
Varma_Chekuri <77021960+cmos1-VC@users.noreply.github.com>
2021-08-30 22:25:47 +0530

    Update 10_ADisplay array values.java

--- Problems/10_ADisplay array values.java
+++ Problems/10_ADisplay array values.java
@@ -45,3 +45,37 @@ public class Array
         }
     }
 }
+
+/* Same question but try printing the values in reverse order*/
+
+import java.util.Scanner;
+
+public class Array
+{
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int[] a;
+        System.out.println("Enter the Array Size");
+        int s = sc.nextInt();
+        a = new int[s];
+        System.out.println("Enter their values");;
+        for(int i = 0; i<a.length;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        for(int j=(a.length)-1;j>=0;j--)
+        {
+            System.out.println(a[j]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
